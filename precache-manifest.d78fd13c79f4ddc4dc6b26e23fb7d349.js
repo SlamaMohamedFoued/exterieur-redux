967208eadc7b0d5bfb283724f46a14faa696204b
SlamaMohamedFoued <medfouedslama@outlook.fr>
2020-11-26 23:09:20 +0100

    Updates

---
+++ precache-manifest.d78fd13c79f4ddc4dc6b26e23fb7d349.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11060452bd6920a19f37cbb1b2f60a19",
+    "url": "/SlamaMohamedFoued/test.git/index.html"
+  },
+  {
+    "revision": "fb96a5be2294840393fe",
+    "url": "/SlamaMohamedFoued/test.git/static/css/main.f6b7f38d.chunk.css"
+  },
+  {
+    "revision": "36ceddc4e4795b2b7c9a",
+    "url": "/SlamaMohamedFoued/test.git/static/js/2.371e0ca8.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/SlamaMohamedFoued/test.git/static/js/2.371e0ca8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb96a5be2294840393fe",
+    "url": "/SlamaMohamedFoued/test.git/static/js/main.e44da20b.chunk.js"
+  },
+  {
+    "revision": "e8fe6cc8d311a54a5386",
+    "url": "/SlamaMohamedFoued/test.git/static/js/runtime-main.ec3944bf.js"
+  },
+  {
+    "revision": "4a5341968b884f77afcae170693c3ecb",
+    "url": "/SlamaMohamedFoued/test.git/static/media/arrowRight.4a534196.svg"
+  },
+  {
+    "revision": "a0d3d32902b4ea3a81b7093166aaf6ee",
+    "url": "/SlamaMohamedFoued/test.git/static/media/bg.a0d3d329.svg"
+  },
+  {
+    "revision": "eb823f15e26a42342ffb6cf85d3d7212",
+    "url": "/SlamaMohamedFoued/test.git/static/media/conseil.eb823f15.png"
+  },
+  {
+    "revision": "7a44cb6f4ade30e6da7b6e16ed0d4a6f",
+    "url": "/SlamaMohamedFoued/test.git/static/media/e-banner.7a44cb6f.svg"
+  },
+  {
+    "revision": "37afca0f998f33131a5f6404b2658056",
+    "url": "/SlamaMohamedFoued/test.git/static/media/fans.37afca0f.svg"
+  },
+  {
+    "revision": "13fb85020800d7bc2a0a7dbc1eb3d9c4",
+    "url": "/SlamaMohamedFoued/test.git/static/media/floor.13fb8502.svg"
+  },
+  {
+    "revision": "62abec4e51a79b1ccbd6e195ae924f49",
+    "url": "/SlamaMohamedFoued/test.git/static/media/gamme1.62abec4e.png"
+  },
+  {
+    "revision": "326b0b4b3f13256d60317004a35af175",
+    "url": "/SlamaMohamedFoued/test.git/static/media/gamme2.326b0b4b.png"
+  },
+  {
+    "revision": "c2dd3e297dd7eb6b67fc1ecc1a3baf62",
+    "url": "/SlamaMohamedFoued/test.git/static/media/gamme3.c2dd3e29.png"
+  },
+  {
+    "revision": "d3cce89107af6911f23a1319efe3da27",
+    "url": "/SlamaMohamedFoued/test.git/static/media/gamme4.d3cce891.png"
+  },
+  {
+    "revision": "4a639bb54fb492732fd2767d2f5a6baf",
+    "url": "/SlamaMohamedFoued/test.git/static/media/gamme5.4a639bb5.png"
+  },
+  {
+    "revision": "a55444c5a14c7f41c6a6f9825fab32a7",
+    "url": "/SlamaMohamedFoued/test.git/static/media/garden.a55444c5.png"
+  },
+  {
+    "revision": "efe1cacde69b9abef0831092eee05ccf",
+    "url": "/SlamaMohamedFoued/test.git/static/media/glass.efe1cacd.svg"
+  },
+  {
+    "revision": "6fd663135da4a5cdc038f0fecdc47fd3",
+    "url": "/SlamaMohamedFoued/test.git/static/media/ground.6fd66313.png"
+  },
+  {
+    "revision": "e4dfdbc7fb9bd5bea6e1e7382fa63035",
+    "url": "/SlamaMohamedFoued/test.git/static/media/manfloor.e4dfdbc7.svg"
+  },
+  {
+    "revision": "3559ccb929e9f73b0348d1f0eb56eb02",
+    "url": "/SlamaMohamedFoued/test.git/static/media/otherstar.3559ccb9.svg"
+  },
+  {
+    "revision": "f014715a8f06b862c1a93a7069c12031",
+    "url": "/SlamaMohamedFoued/test.git/static/media/stairs.f014715a.svg"
+  },
+  {
+    "revision": "6ccd7c9ec90203fa0da3a6c88c54c1d9",
+    "url": "/SlamaMohamedFoued/test.git/static/media/toolbox.6ccd7c9e.svg"
+  },
+  {
+    "revision": "5fb363b1e259ddea260cb4150bd4d730",
+    "url": "/SlamaMohamedFoued/test.git/static/media/tools.5fb363b1.svg"
+  },
+  {
+    "revision": "8c63b6960020715fe8c3a2e58700ff8d",
+    "url": "/SlamaMohamedFoued/test.git/static/media/working.8c63b696.svg"
+  }
+]);
